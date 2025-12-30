4128abf2d041951fa72f28d85b7e638b8e954423
Niincheen <168728913+Niincheen@users.noreply.github.com>
2025-12-30 18:59:03 +0100

    Button angepasst mit inclick etc.

--- protocol-blackout/src/components/button.jsx
+++ protocol-blackout/src/components/button.jsx
@@ -1,11 +1,23 @@
 // src/components/Button.jsx
 import "./button.css";
 
-function Button() {
+function Button({
+  children,
+  onClick,
+  type = "button",
+  className = "",
+  disabled = false,
+  ...rest
+}) {
   return (
-
-    <button class="button">
-      <span>Login</span>
+    <button
+      type={type}
+      className={`retroBtn ${className}`.trim()}
+      onClick={onClick}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
     </button>
   );
 }
