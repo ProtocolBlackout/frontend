c970c4d3920dbf4b892483a60087351080d5c549
Niincheen <168728913+Niincheen@users.noreply.github.com>
2026-01-03 15:32:18 +0100

    Content eingefügt

--- protocol-blackout/src/pages/ethik/EthischesHacken.jsx
+++ protocol-blackout/src/pages/ethik/EthischesHacken.jsx
@@ -18,18 +18,16 @@ function EthischesHacken() {
             <div className="ethics-console__row">
               <span className="ethics-console__label">HACKER_ETHIK</span>
               <span className="ethics-console__value">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                ultrices, nulla sed tincidunt tempus, lorem nisi ultrices arcu,
-                non hendrerit arcu elit sit amet lorem.
+                Hacker-Ethik beschreibt die moralischen Grundwerte, nach denen verantwortungsbewusste Hacker handeln. Ziel ist es, Sicherheitslücken aufzudecken und Systeme zu verbessern – nicht, Schaden zu verursachen oder sich zu bereichern.
+
               </span>
             </div>
 
             <div className="ethics-console__row">
               <span className="ethics-console__label">RAHMEN</span>
               <span className="ethics-console__value">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed
-                non risus at odio gravida interdum.
+                Ethisches Hacken findet immer in einem klar definierten rechtlichen und organisatorischen Rahmen statt. Dazu gehören Genehmigungen, festgelegte Testziele und die Einhaltung von Gesetzen und Verträgen.
+
               </span>
             </div>
           </div>
@@ -43,7 +41,7 @@ function EthischesHacken() {
                   ZUGANG_NUR_MIT_EINWILLIGUNG
                 </span>
                 <span className="ethics-list__value">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Systeme dürfen ausschließlich mit ausdrücklicher Erlaubnis des Eigentümers getestet werden.
                 </span>
               </li>
               <li>
@@ -51,25 +49,25 @@ function EthischesHacken() {
                   TRANSPARENZ_UND_PROTOKOLLE
                 </span>
                 <span className="ethics-list__value">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vorgehen, Methoden und Ergebnisse müssen nachvollziehbar dokumentiert werden.
                 </span>
               </li>
               <li>
                 <span className="ethics-list__key">KEIN_SCHADEN</span>
                 <span className="ethics-list__value">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tests dürfen den Betrieb von Systemen nicht absichtlich beeinträchtigen oder zerstören.
                 </span>
               </li>
               <li>
                 <span className="ethics-list__key">VERTRAULICHER_UMGANG</span>
                 <span className="ethics-list__value">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Gefundene Schwachstellen und sensible Daten müssen vertraulich behandelt werden.
                 </span>
               </li>
               <li>
                 <span className="ethics-list__key">VERANTWORTUNG</span>
                 <span className="ethics-list__value">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Ethische Hacker tragen Verantwortung für die Folgen ihres Handelns und ihrer Erkenntnisse.
                 </span>
               </li>
               <li>
@@ -77,7 +75,7 @@ function EthischesHacken() {
                   LERNEN_UND_TEILEN_MIT_SINN
                 </span>
                 <span className="ethics-list__value">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Wissen soll zur Verbesserung der Sicherheit beitragen – nicht zum Missbrauch.
                 </span>
               </li>
             </ul>
@@ -91,15 +89,13 @@ function EthischesHacken() {
                 <h3 className="ethics-split__heading">ERLAUBT</h3>
                 <ul className="ethics-split__list">
                   <li>
-                    Tests nur im vereinbarten Rahmen durchführen – Lorem ipsum
-                    dolor sit amet.
+                    Tests nur im vereinbarten Rahmen durchführen und vereinbarte Grenzen einhalten.
                   </li>
                   <li>
-                    Ergebnisse sauber dokumentieren – Lorem ipsum dolor sit amet.
+                    Ergebnisse strukturiert dokumentieren und nachvollziehbar aufbereiten.
                   </li>
                   <li>
-                    Schwachstellen verantwortungsvoll melden – Lorem ipsum dolor
-                    sit amet.
+                    Schwachstellen verantwortungsvoll melden, bevor sie öffentlich gemacht werden.
                   </li>
                 </ul>
               </div>
@@ -107,9 +103,9 @@ function EthischesHacken() {
               <div className="ethics-split__column ethics-split__column--dont">
                 <h3 className="ethics-split__heading">NICHT_ERLAUBT</h3>
                 <ul className="ethics-split__list">
-                  <li>UNBEFUGTER_ZUGRIFF – Lorem ipsum dolor sit amet.</li>
-                  <li>DATENDIEBSTAHL_ODER_LEAKS – Lorem ipsum dolor sit amet.</li>
-                  <li>ABSICHTLICHER_SCHADEN – Lorem ipsum dolor sit amet.</li>
+                  <li>UNBEFUGTER_ZUGRIFF – Eindringen in Systeme ohne Zustimmung des Eigentümers.</li>
+                  <li>DATENDIEBSTAHL_ODER_LEAKS – Kopieren, Veröffentlichen oder Weitergeben sensibler Daten.</li>
+                  <li>ABSICHTLICHER_SCHADEN – Sabotage, Erpressung oder Störung von Systemen.</li>
                 </ul>
               </div>
             </div>
