75e661dea760ee5b94c58cbeb5d4323e790dae9e
Michelle A. <loyalslytherindeatheater@gmail.com>
2026-01-27 19:24:10 +0100

    feature: Impressum befüllt und Disclaimer in Footer hinzugefügt

--- protocol-blackout/src/components/Footer.jsx
+++ protocol-blackout/src/components/Footer.jsx
@@ -4,8 +4,9 @@ function Footer() {
   return (
     <footer className="pb-footer" id="...">
       <span>Team Protocol Blackout</span>
-      <span>©2023</span>
+      <span>©2025</span>
       <span>Follow Us: [Social Icons]</span>
+      <span>Diese Website ist ein reines Schulungsprojekt, das im Rahmen einer Ausbildung zur Web- und Softwareentwicklung erstellt wurde. Alle Inhalte, Produkte und Dienstleistungen dienen ausschließlich Demonstrations- und Übungszwecken. Es besteht keine Absicht, gewerbliche Handlungen vorzunehmen oder echte Verträge zu schließen. Personenbezogene Daten (falls erhoben) werden nicht dauerhaft gespeichert oder an Dritte weitergegeben, sofern nicht anders angegeben.</span>
     </footer>
   );
 }
